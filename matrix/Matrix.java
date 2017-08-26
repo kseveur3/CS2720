3bc19b6012634fad9f0025a54a08b3fdab6f0366
kseveur3 <kseveur3@gatech.edu>
2017-08-26 13:46:56 -0400

    New Version

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -11,37 +11,68 @@ import java.util.Scanner;
  * 3)Data Structures Used: None
  * 4)How to use program and expected input/output: The program will prompt
  *   the user for an integer. This integer will be used to create a matrix of
- *   random values. This created matrix will be used in an addtion and a 
+ *   random values. This created matrix will be used in an addtion and a
  *   multiplication formula to solve.
  * 5)Purpose of each class:
  *   Matrix Class - This is the program
  *   MatrixMath Class - Used to hold all the methods to create random values
- *      for matrices, copy matrix one into matrix2, add the matrices, and 
+ *      for matrices, copy matrix one into matrix2, add the matrices, and
  *      multiply the matrices.
  */
-        
 
+/**
+ * ProgramtTo request a matrix size from the user then add and/or
+ * multiply the two matrices together.
+ * @author kevinseveur
+ */
 public class Matrix {
 
+    /**
+     * Main program.
+     * @param args
+     */
     public static void main(String[] args) {
-        
-        
         Scanner in = new Scanner(System.in);
-        System.out.print("Please enter a single integer to be used as your "
-                + "matrix dimensions: ");
-        if(!in.hasNextInt()){
-            System.out.println("You did not enter an integer!");
-            return;
+        int usrInput;
+        String usrInput2;
+        boolean repeat = true;
+
+        while (repeat) {
+            repeat = false;
+            System.out.println();
+            System.out.print("Please enter a single integer to be used as your "
+                    + "matrix dimensions (minimum of 5): ");
+            while (!in.hasNextInt()) {
+                System.out.println("You did not enter an integer! "
+                        + "Please try again!");
+                in.next();
+            }
+
+            usrInput = in.nextInt();
+
+            while (usrInput < 5) {
+                System.out.println("Please enter a value greater or equal to 5!");
+                usrInput = in.nextInt();
+            }
+            int[][] matrixShell = new int[usrInput][usrInput];
+            int[][] matrix1 = MatrixMath.addValues(matrixShell);
+            int[][] matrix2 = MatrixMath.arrayCopy(matrix1);
+            MatrixMath.matrixAdd(matrix1, matrix2, usrInput);
+            MatrixMath.matrixmult(matrix1, matrix2, usrInput);
+
+            System.out.println("Would you like to repeat the program"
+                    + " (yes or no)?");
+            usrInput2 = in.next();
+
+            while (!usrInput2.equals("yes") && !usrInput2.equals("no")) {
+                System.out.println("Please enter \"yes\" or \"no\"");
+                usrInput2 = in.next();
+            }
+            if (usrInput2.equals("yes")) {
+                repeat = true;
+            } else {
+                repeat = false;
+            }
         }
-        int usrInput = in.nextInt();
-        int matrix[][] = new int[usrInput][usrInput];
-        
-        int matrix1[][] = MatrixMath.addValues(matrix);
-        int matrix2[][] = MatrixMath.arrayCopy(matrix1);
-        MatrixMath.matrixAdd(matrix1, matrix2, usrInput);
-        MatrixMath.matrixmult(matrix1, matrix2, usrInput);
-        
-        
     }
-    
 }
--- matrix/MatrixMath.java
+++ matrix/MatrixMath.java
@@ -4,49 +4,51 @@ import java.sql.Timestamp;
 import java.util.Random;
 
 /**
- *
+ *This class is used to perform addition or multiplication on matrices.
  * @author kevinseveur
  */
 public class MatrixMath {
     /**
-     * This method creates random integers and adds them to the matrix
+     * This method creates random integers and adds them to the matrix.
      * @param m1 This is the matrix being passed in
      * @return Returns a matrix with random integers filled in
      */
     public static int[][] addValues(int[][] m1) {
        int[][] matrix = new int[m1.length][m1.length];
-       System.out.println("\nHere is your randomly generated matrix: ");
-       for(int row = 0; row < m1.length; row++) {
-           for(int column = 0; column < m1[row].length; column++) {
+       System.out.println("\nHere is your first randomly generated matrix: ");
+       for (int row = 0; row < m1.length; row++) {
+           for (int column = 0; column < m1[row].length; column++) {
              Random rand = new Random();
-             int  rval = rand.nextInt(50) + 1;
-             matrix[row][column] = rval; 
+             int rval = rand.nextInt(30) + 1;
+             matrix[row][column] = rval;
              System.out.printf("%6s", matrix[row][column]);
-            }  
+            }
            System.out.println();
        }
        return matrix;
     }
     /**
-     * This method only copies the contents of each element in the matrix 
-     * to a second matrix
+     * This method only copies the contents of each element in the matrix
+     * to a second matrix.
      * @param m1 The original matrix of which we are copying from
      * @return returns a new second matrix which is a copy of the passed in
      * matrix
      */
     public static int[][] arrayCopy(int[][] m1) {
        int[][] matrix = new int[m1.length][m1.length];
-       
-       for(int row = 0; row < m1.length; row++) {
+       System.out.println("\nHere is your second randomly generated matrix: ");
+       for (int row = 0; row < m1.length; row++) {
            for(int column = 0; column < m1[row].length; column++) {
-              matrix[row][column] = m1[row][column]; 
-            }   
+              matrix[row][column] = m1[row][column];
+              System.out.printf("%6s", matrix[row][column]);
+            }
+           System.out.println();
        }
        return matrix;
     }
-    
+
     /**
-     * This method adds the two matrices together
+     * This method adds the two matrices together.
      * @param m1 This is the first matrix of which we are adding
      * @param m2 This is the second matrix of which we are adding
      * @param size This is the original matric size passed in by the user
@@ -54,26 +56,29 @@ public class MatrixMath {
     public static void matrixAdd(int[][] m1, int[][] m2, int size) {
         Timestamp timestamp1 = new Timestamp(System.currentTimeMillis());
         int[][] addSolution = new int[size][size];
-        System.out.println("\nHere is your added solution: " );
-        for(int row = 0; row < m1.length; row++) {
-            for(int column = 0; column < m1[row].length; column++) {
+        System.out.println("------------------------------------"
+                + "--------------");
+        System.out.println("\nHere is your added solution: ");
+        for (int row = 0; row < m1.length; row++) {
+            for (int column = 0; column < m1[row].length; column++) {
               addSolution[row][column] = m1[row][column] + m2[row][column];
-              
-              System.out.printf("%6s", +addSolution[row][column]); 
+
+              System.out.printf("%6s", +addSolution[row][column]);
             }
             System.out.println();
         }
         Timestamp timestamp2 = new Timestamp(System.currentTimeMillis());
         long millis1 = timestamp1.getTime();
         long millis2 = timestamp2.getTime();
-        double duration = millis2-millis1;
-        System.out.printf("\nRunning Time for MatrixAdd in milliseconds:%.4f "
-                , duration);
+        double duration = millis2 - millis1;
+        System.out.printf("\nRunning Time for MatrixAdd in milliseconds:%.4f ",
+                duration);
+        System.out.println();
         System.out.println();
     }
-    
+
     /**
-     * This method multiplies the matrices together
+     * This method multiplies the matrices together.
      * @param m1 This is the first matrix of which we are multiplying
      * @param m2 This is the second matrix of which we are multiplying
      * @param size This is the original matric size passed in by the user
@@ -82,27 +87,29 @@ public class MatrixMath {
         Timestamp timestamp1 = new Timestamp(System.currentTimeMillis());
         int[][] addSolution = new int[size][size];
         int value = 0;
-        System.out.println("--------------------------------------------------");
-        System.out.println("Here is your multiplied solution: " );
-        
-        for(int i = 0; i < m1.length; i++) {
-            for(int j = 0; j < m1.length; j++) {
-              for(int k = 0; k < m1.length; k++ ) {
+        System.out.println("------------------------------------"
+                + "--------------");
+        System.out.println("Here is your multiplied solution: ");
+
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1.length; j++) {
+              for (int k = 0; k < m1.length; k++) {
                   value += m1[i][k] * m2[k][j];
               }
               addSolution[i][j] = value;
               value = 0;
-              
-              System.out.printf("%6s", +addSolution[i][j]); 
+
+              System.out.printf("%6s", +addSolution[i][j]);
             }
             System.out.println();
         }
         Timestamp timestamp2 = new Timestamp(System.currentTimeMillis());
         long millis1 = timestamp1.getTime();
         long millis2 = timestamp2.getTime();
-        double duration = millis2-millis1;
-        System.out.printf("\nRunning Time for MatrixMult in milliseconds:%.4f "
-                , duration);
+        double duration = millis2 - millis1;
+        System.out.printf("\nRunning Time for MatrixMult in milliseconds:%.4f ",
+                duration);
         System.out.println();
-    }  
+        System.out.println();
+    }
 }
