92563a1eff5dd4e511d1b9e74e9651fdc0e4e06e
kseveur3 <kseveur3@gatech.edu>
2017-09-06 20:19:36 -0400

    New Files

---
+++ elevator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Elevator;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kevinseveur
+ */
+public class elevator {
+
+    final int numberOfFloors = 12; //number of floorsin building
+    final int numberOfStopsUp = 8; //number of initial stops going up
+    final int numberOfStopsDown = 5; // number of initial stos going down
+    ArrayList<Integer> goingUp = new ArrayList<Integer>();
+    ArrayList<Integer> goingDown = new ArrayList<Integer>();
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
