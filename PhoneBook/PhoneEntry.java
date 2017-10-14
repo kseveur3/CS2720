22a392644eac8fc38461ce8836204b8a8083bb37
kseveur3 <kseveur3@gatech.edu>
2017-10-14 12:21:06 -0400

    Update PhoneEntry.java

--- PhoneBook/PhoneEntry.java
+++ PhoneBook/PhoneEntry.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package phonedir;
+
 
 import java.util.LinkedList;
 
@@ -17,10 +17,10 @@ public class PhoneEntry {
     private String lname;
     private String phone;
     private int entry;
-    
+
     /**
-     * Precondition - 
-     * Postcondition - 
+     * Precondition -
+     * Postcondition -
      */
     public PhoneEntry() {
         fname = null;
@@ -29,98 +29,78 @@ public class PhoneEntry {
     }
 
     /**
-     * Precondition - 
-     * Postcondition - 
+     * Precondition -
+     * Postcondition -
      */
     public PhoneEntry(String first, String last, String phoneNumber) {
-        fname = first;
-        lname = last;
+        setFirst(first);
+        setLast(last);
         phone = phoneNumber;
     }
 
     /**
-     * Precondition - 
-     * Postcondition - 
+     * Precondition -
+     * Postcondition -
      */
     public String getName() {
         return fname + " " + lname;
     }
-    
+
     public String getFirst() {
         return fname;
     }
-    
+
     public String getLast() {
         return lname;
     }
 
     /**
-     * Precondition - 
-     * Postcondition - 
+     * Precondition -
+     * Postcondition -
      */
     public void setFirst(String first) {
-        fname = first;  
+        String f = first.toLowerCase();
+        Character fi = Character.toUpperCase(f.charAt(0));
+        fname = fi + f.substring(1,f.length());
     }
-    
+
     public void setLast( String last) {
-        lname = last;
+        String l = last.toLowerCase();
+        Character lo = Character.toUpperCase(l.charAt(0));
+        lname = lo + l.substring(1,l.length());
     }
 
     /**
-     * Precondition - 
-     * Postcondition - 
+     * Precondition -
+     * Postcondition -
      */
     public String getPhone() {
         return phone.substring(0,3) + "-" + phone.substring(3,6) + "-" +
                 phone.substring(6,phone.length());
     }
-    
+
+    public String getPhoneNoFormat() {
+        return phone;
+    }
+
     public int getElement(LinkedList<PhoneEntry> list, String first, String last) {
-       int idx = -1;
-       
+        int idx = -1;
+
         for (PhoneEntry p : list) {
             idx++;
             if(p.fname.equals(first) && p.lname.equals(last)) {
                 return idx;
-            } 
+            }
         }
         return -1;
     }
 
     /**
-     * Precondition - 
-     * Postcondition - 
+     * Precondition -
+     * Postcondition -
      */
     public void setPhone(String newPhone) {
         phone = newPhone;
     }
-    
-    /**
-     * Precondition - 
-     * Postcondition - 
-     */
-    public void display(LinkedList<PhoneEntry> list) {
-        for (PhoneEntry p: list) {
-                    System.out.printf("Name : %-20s PhoneNo :%1s\n\n",
-                            p.getName(), p.getPhone());
-        } 
-    }
 
-    
-    
-    /*@Override
-	public int compareTo(PhoneEntry o) {
-            if (this.name.compareTo(o.name) != 0) {
-                return this.name.compareTo(o.name);
-            }
-            int comparedPhone = Integer.parseInt(o.phone);
-            int currentPhone = Integer.parseInt(this.phone);
-		if (currentPhone > comparedPhone) {
-			return 1;
-		} else if (currentPhone == comparedPhone) {
-			return 0;
-		} else {
-			return -1;
-		}
-        }*/
 }
