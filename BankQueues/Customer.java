8e5638a5ab2d6031f313f90eb5d10ef5e1c289f2
kseveur3 <kseveur3@gatech.edu>
2017-11-11 20:41:54 -0500

    Update Customer.java

--- BankQueues/Customer.java
+++ BankQueues/Customer.java
@@ -1,35 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bank;
 import java.lang.*;
 
+ /**
+ *1) Class Purpose
+ *      The Customer class hold the Customer object for use by the Bank
+ *      main program.
+ */
 public class Customer {
-    
+
     protected int arrivalTime;
     protected int processTime;
-    
+    public int initialTellerTime;
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public Customer(int at) {
         arrivalTime = at;
-        processTime = 2 + (int)(Math.random() * 6);
+        processTime = 2 + (int) (Math.random() * 6);
     }
-    
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public Customer() {
         arrivalTime = 0;
-        processTime = 0;       
+        processTime = 0;
     }
-    
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public boolean done() {
         return --processTime < 0;
     }
-    
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public int arrival() {
         return arrivalTime;
     }
-    public int getProcessTime() {
-    	return processTime;
-    }
     
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
+    public int getProcessTime() {
+        return processTime;
+    }
 }
