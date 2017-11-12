734c0b0ea14272cff4a34dcee5cdda6dd0f8842c
kseveur3 <kseveur3@gatech.edu>
2017-11-11 20:41:36 -0500

    Update Teller.java

--- BankQueues/Teller.java
+++ BankQueues/Teller.java
@@ -1,55 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bank;
 
 /**
- *
- * @author kevinseveur
+ *1) Class Purpose
+ *   The Teller class hold the Teller object for use by the Bank main program.
  */
 public class Teller {
-    protected boolean free;
+    protected boolean free; 
     protected Customer customer;
     protected int customerCount = 0;
     protected int customerTime = 0;
-    
-    
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public Teller() {
         free = true;
     }
-    
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public Teller(int time) {
         Customer cust = new Customer(time);
-        addCustomer(cust, cust.getProcessTime());
+        customerTime = cust.getProcessTime();
     }
-    
-    
-    public boolean isFree() {
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
+    public boolean isFree(int currentClock) {
         if (free) {
             return true;
-        }
-        if (customer.done()) {
+        } else if (currentClock >= (customer.initialTellerTime
+                + customer.getProcessTime())) {
             free = true;
         }
         return free;
     }
-    
-    public void addCustomer(Customer c, int t) {
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
+    public void addCustomer(Customer c) {
         customer = c;
         free = false;
-        customerCount += 1;
-        customerTime += t;
-        
-        
-    }  
-    
-    public int timeTotal() {
-    	return customerTime;
+        customerCount++;
+        customerTime += c.getProcessTime();
     }
 
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
+    public int timeTotal() {
+        return customerTime;
+    }
+
+    /**
+     * Precondition - 
+     * Postcondition - 
+     */
     public int getCustomerCount() {
-    	return customerCount;
+        return customerCount;
     }
 }
